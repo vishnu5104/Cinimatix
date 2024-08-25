51828b77780d8087fa1a18cabd031ad31d9f47dc
Vishnu S <86919026+vishnu5104@users.noreply.github.com>
2024-08-25 16:11:53 +0530

    Updates hardhat.config.ts

--- src/blockchain/hardhat.config.ts
+++ src/blockchain/hardhat.config.ts
@@ -4,7 +4,7 @@ import "@nomicfoundation/hardhat-toolbox";
 const ALCHEMY_API_KEY = "pZrY82LiRq-ebekynxrUcYzSdtPfDPEn";
 
 const SEPOLIA_PRIVATE_KEY =
-  "3419239e818b7aaa3bd6293b6ad34f0efba02cfbefb6933144548dfd09bea8ae";
+  "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
